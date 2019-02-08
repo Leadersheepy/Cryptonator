dcb6d54fd5536f1085e788e52ee7506990ad0c8f
Leadersheepy <marine.kazemi@gmail.com>
2019-02-08 10:32:32 +0100

    Ajout du déchiffrement 2

--- Crypto1.py
+++ Crypto1.py
@@ -50,23 +50,42 @@ http://naelshiab.com/python-et-la-cryptographie-et-un-peu-de-piratage-aussi/?fbc
 #                print(cle)
 #    
 
-fichier = open("message1.txt", encoding="utf-8")
-Mot=fichier.read()
-#Mot= "JuL dreioVet srom.s dlo "
-Taille=len(Mot)
-M=int(input('Taille:'))
-b = True
-for cle in range(2,M):
-    Nouv=""
-    for i in range(cle):
-        for j in range(i,Taille,cle):
-            Nouv=Nouv+Mot[j]
-#        print(cle)
+#fichier = open("message1.txt", encoding="utf-8")
+#Mot=fichier.read()
+##Mot= "JuL dreioVet srom.s dlo "
+#Taille=len(Mot)
+#M=int(input('Taille:'))
+#b = True
+#for cle in range(2,M):
+#    Nouv=""
+#    for i in range(cle):
+#        for j in range(i,Taille,cle):
+#            Nouv=Nouv+Mot[j]
+##        print(cle)
+##        print(Nouv[:15])
+#    if Nouv[:2]=="Je":
+#        b = False
 #        print(Nouv[:15])
-    if Nouv[:2]=="Je":
-        b = False
-        print(Nouv[:15])
-        print(cle)
+#        print(cle)
+#
+#if b:
+#    print("il n'y a pas :(")  
 
-if b:
-    print("il n'y a pas :(")    
+#----------------------------------MESSAGE 2----------------------------------#  
+fichier = open("message2.txt", encoding="utf-8")
+Mot=fichier.read()
+#Mot=""
+Taille=len(Mot)
+Borne=int(input('Chiffre de César:'))
+Chiffre=0
+Nombre=0
+for decalage in range (Borne):
+    Nouv=""
+    for i in range(Taille):
+        Chiffre=ord(Mot[i])
+        Nombre=chr(Chiffre-decalage)
+        Nouv=Nouv+Nombre
+    if Nouv[-4:]=="Joël":
+        b = False
+        print(decalage)
+        print(Nouv)
