7686357cbea0d6641f315acb79694b26b5c1f36d
Leadersheepy <marine.kazemi@gmail.com>
2019-04-19 02:37:22 +0200

    Update Crypto1.py

--- Crypto1.py
+++ Crypto1.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 """
+    
+##-------------------------------------------------
+##-------------------------------------------------
+#                   TABLEAU
+##-------------------------------------------------
+##-------------------------------------------------
+
 #-------------------------------------------------
 #               MESSAGE 1 VERS1
 #-------------------------------------------------
 #
-"""fichier = open("message1.txt", encoding="utf-8")
-Mot=fichier.read()"""
-Mot= "JuL dreioVet srom.s dlo "
+fichier = open("message1.txt", encoding="utf-8")
+Mot=fichier.read()
+#Mot= "JuL dreioVet srom.s dlo "
 Taille=len(Mot)
 M=int(input('Taille:'))
 b = True
@@ -49,6 +56,7 @@ while i<M and Nouv[:14]!= "Fonctionnement":
                 print(Nouv[:15])
                 print(cle)
     
+##-------------------------------------------------
 
 fichier = open("message1.txt", encoding="utf-8")
 Mot=fichier.read()
@@ -70,6 +78,14 @@ for cle in range(2,M):
 
 if b:
     print("il n'y a pas :(")  
+    
+##-------------------------------------------------
+##-------------------------------------------------
+#                   CESAR
+##-------------------------------------------------
+##-------------------------------------------------
+
+ 
 #
 ##-------------------------------------------------
 ##               MESSAGE 2 & 3 Grâce à un mot
@@ -79,8 +95,7 @@ Mot=fichier.read()
 Mot=""
 Taille=len(Mot)
 Borne=int(input('Chiffre de César:'))
-Chiffre=0
-Nombre=0
+Chiffre=0 #Variable qui représente la valeur de la lettre dans le dic pointé par i
 for decalage in range (Borne):
     Nouv=""
     for i in range(Taille):
@@ -99,21 +114,23 @@ fichier = open("message2.txt", encoding="utf-8")
 Mot=fichier.read()
 #Mot=""
 Taille=len(Mot)
-Dic={}
-Chiffre=0
-Nombre=0
-Borne=0
+Chiffre=0 #Variable qui représente la valeur de la lettre dans le dic pointé par i
+Decal=0 #Variable qui va servir à changer la valeur de la lettre 
+Dic={} #Création de dictionnaires pour la clé
+Borne=0 #Variable trouver grâce au dic
 for c in Mot:
     if c in Dic:
         Dic[c]=Dic[c]+1
     else:
         Dic[c]=1
 print(Dic)
-#Borne=int(input('Chiffre de César:'))
+#Ici il faudra choisir sur quel lettre vous voulez utiliser comme base pour le code
 Lettre=input('Tu veux te baser sur quel lettre ?:')
 L=ord(Lettre)
 print(L)
 
+#Ici il faudra choisir en quoi vous voulez transformer la lettre ex: je sais que la derniére
+#Lettre sera un l donc je dis à mon programme qu'il doit transformer ma lettre "e" en "l"
 Chi=input('Tu veux la transformer en quoi ?:')
 C=ord(Chi)
 
@@ -124,26 +141,35 @@ Nouv=""
 for i in range(Taille):
     Chiffre=ord(Mot[i])
     Decal=chr(Chiffre-Borne)
-    #Nombre=chr(((Chiffre-ord('a')+Borne)%26)+ord('a'))
     Nouv=Nouv+Decal
 print(Nouv)
+
+
+    
+##-------------------------------------------------
+##-------------------------------------------------
+#                   CESAR AVEC 2 CLES
+##-------------------------------------------------
+##-------------------------------------------------
+
 #
 ##-------------------------------------------------
 ##               MESSAGE 4 qui fonctionne mais pas sur spyder
 ##-------------------------------------------------
 ##23    
 ##Mot="""7aĂ"""
-#fichier = open("message4.txt", encoding="utf-8")
-#Mot=fichier.read()
+fichier = open("message4.txt", encoding="utf-8")
+Mot=fichier.read()
 #fichier.close()
 Taille=len(Mot)
-Dic={}
-Dic2={}
-Chiffre=0
-Nombre=0
-Borne=0
-d=0
-e=0
+Chiffre=0 #Variable qui représente la valeur de la lettre dans le dic pointé par i
+Decal=0 #Variable qui va servir à changer la valeur de la lettre 
+Dic={} #Création de dictionnaires pour les 2 clés
+Dic2={} #Création de dictionnaires pour les 2 clés
+Borne=0 #Variable trouver grâce au dic
+d=0 #Variable pour les boucles
+e=0 #Variable pour les boucles
+# Code pour séparer les Dic en 2 parties (d'un côté les pairs et l'autres les impairs)
 for c in Mot:
     d=d+1
     if d % 2 == 0 :
@@ -164,26 +190,34 @@ for x in Dic:
 print("-----------------------------------")
 
 for y in Dic2:   
-    print (y,':',Dic2[y])        
+    print (y,':',Dic2[y])    
+#-------------------------------
 
+#Ici il faudra choisir sur quel lettre vous voulez utiliser comme base pour le code
 Lettre=input('1-Tu veux te baser sur quel lettre ?:')
 L=ord(Lettre)
 print(L)
+#Ici il faudra choisir en quoi vous voulez transformer la lettre ex: je sais que la derniére
+#Lettre sera un l donc je dis à mon programme qu'il doit transformer ma lettre "e" en "l"
 Chi=input('1-Tu veux la transformer en quoi ?:')
 C=ord(Chi)
 Borne= C-L
 print(Borne)
 
+#Ici il faudra choisir sur quel lettre vous voulez utiliser comme base pour le code
 Lettre=input('2-Tu veux te baser sur quel lettre ?:')
 L=ord(Lettre)
 print(L)
+#Ici il faudra choisir en quoi vous voulez transformer la lettre ex: je sais que la derniére
+#Lettre sera un l donc je dis à mon programme qu'il doit transformer ma lettre "e" en "l"
 Chi=input('2-Tu veux la transformer en quoi ?:')
 C=ord(Chi)
 Borne2= C-L
 print(Borne2)
 Nouv=""
 
-for i in range(Taille):
+for i in range(Taille):#Sépare le tri en 2 parties et que chaque borne agis sur une partie
+               #différente
     if i%2==0:
         Chiffre=ord(Mot[i])
         Decal=chr(Chiffre+Borne)
@@ -194,19 +228,29 @@ for i in range(Taille):
         Nouv=Nouv+Decal
 print(Nouv[:20])
 
+    
+##-------------------------------------------------
+##-------------------------------------------------
+#                   CESAR AVEC 3 CLES
+##-------------------------------------------------
+##-------------------------------------------------
 #-------------------------------------------------
 #               MESSAGE 5 
 #-------------------------------------------------
 mon_fichier = open("message5.txt","r",encoding="utf8")
 Mot = mon_fichier.read()
 #Mot = "Lxîn"
-Dic1={}
-Dic2={}
-Dic3={}
-d=0
-f=0
-g=0
-e=0
+Chiffre=0 #Variable qui représente la valeur de la lettre dans le dic pointé par i
+Decal=0 #Variable qui va servir à changer la valeur de la lettre 
+Dic1={} #Création de dictionnaires pour les 3 clés
+Dic2={} #Création de dictionnaires pour les 3 clés
+Dic3={} #Création de dictionnaires pour les 3 clés
+d=0 #Variable pour les boucles
+f=0 #Variable pour les boucles
+g=0 #Variable pour les boucles
+e=0 #Variable pour les boucles
+#-----------------------------
+#Mis en place des 3 Dic
 for c in Mot:
     d=d+1
     if d % 3 == 0 :
@@ -240,33 +284,45 @@ print("-----------------------------------")
 
 for z in Dic3:   
     print (z,':',Dic3[z])
-
+#-----------------------------
+ 
+#Ici il faudra choisir sur quel lettre vous voulez utiliser comme base pour le code
 Lettre=input('1-Tu veux te baser sur quel lettre ?:')
 L=ord(Lettre)
 print(L)
+#Ici il faudra choisir en quoi vous voulez transformer la lettre ex: je sais que la derniére
+#Lettre sera un l donc je dis à mon programme qu'il doit transformer ma lettre "e" en "l"
 Chi=input('1-Tu veux la transformer en quoi ?:')
 C=ord(Chi)
 Borne= L-C
 print(Borne)
 
+#Ici il faudra choisir sur quel lettre vous voulez utiliser comme base pour le code
 Lettre=input('2-Tu veux te baser sur quel lettre ?:')
 L=ord(Lettre)
 print(L)
+#Ici il faudra choisir en quoi vous voulez transformer la lettre ex: je sais que la derniére
+#Lettre sera un l donc je dis à mon programme qu'il doit transformer ma lettre "e" en "l"
 Chi=input('2-Tu veux la transformer en quoi ?:')
 C=ord(Chi)
 Borne2= L-C
 print(Borne2)
 
+#Ici il faudra choisir sur quel lettre vous voulez utiliser comme base pour le code
 Lettre=input('3-Tu veux te baser sur quel lettre ?:')
 L=ord(Lettre)
 print(L)
+#Ici il faudra choisir en quoi vous voulez transformer la lettre ex: je sais que la derniére
+#Lettre sera un l donc je dis à mon programme qu'il doit transformer ma lettre "e" en "l"
 Chi=input('3-Tu veux la transformer en quoi ?:')
 Borne3= L-C
 print(Borne3)
 Nouv=""
+
 for i in Mot:
     e=e+1
-    if e%3==0:
+    if e%3==0: #Code pour séparer le tri en 3 parties et que chaque borne agis sur une partie
+               #différente
         Chiffre=ord(i)
         Decal=chr(Chiffre-Borne)
         Nouv=Nouv+Decal
